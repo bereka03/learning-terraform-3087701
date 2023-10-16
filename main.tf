0861d4a9a5942fc92c988591367d3340bb2d0c9b
Giorgi <93348926+bereka03@users.noreply.github.com>
2023-10-16 15:14:16 +0400

    fix errors try_3

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -47,7 +47,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog"
 
-  vpc_id = module.blog_vpc.vpc_id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
